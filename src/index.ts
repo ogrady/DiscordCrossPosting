acea5da6fbf2c6e732e14369644affd010a88c70
Daniel O'Grady <danielogrady@gmx.de>
2022-07-23 15:03:07 +0200

    Add register command line args
    Also fix a bug where bot would log in twice.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import config from '../config.json'
+import * as CommandLineArgs from "command-line-args"
 import { GatewayIntentBits } from 'discord.js'
 import { BotClient } from './bot-client'
 
-const client = new BotClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], dbfile: './db/database.db' })
+const args = CommandLineArgs.default([
+    { name: "register", alias: "r", type: Boolean, default: false }
+]);
 
-client.once('ready', () => {
-    console.log('Starting up...')
-    client.login(config.token)
-    console.log('Bot started.')
-})
+const client = new BotClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], dbfile: './db/database.db' })
 
 const shutdown = () => {
     console.info('SIGTERM signal received.')
@@ -19,34 +18,16 @@ const shutdown = () => {
     process.exit(0)
 }
 
-const main = async () => {
-    //await client.reregisterCommands()
+const main = async (args) => {
+    if (args.register) {
+        await client.reregisterCommands()
+    }
+    console.log('Starting up...')
     client.login(config.token)
+    console.log('Bot started.')
 }
 
 
 ['SIGTERM', 'SIGINT'].forEach(value => process.on(value, shutdown))
 
-main()
-
-
-/*
-
-    const client = new BotClient({
-        ownerID: config.owner_ids,
-        prefix: config.prefix,
-        commandDirectory: './built/commands/',
-        //inhibitorDirectory: "./built/inhibitors/",
-        listenerDirectory: './built/listeners/',
-        commandUtil: true,
-        commandUtilLifetime: 600000
-    }, './db/database.db')
-
-
-
-
-
-
-
-
-*/
+main(args)
